cf01efdc7bac3d380c3eab9674166daa9624563b
konoplitsky <lavkraftandrusa@gmail.com>
2026-04-07 16:39:58 +0500

    major: update config

--- release.config.js
+++ release.config.js
@@ -2,19 +2,45 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 
+const noteKeywords = ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+
+const releaseRules = [
+    // Hidden general semver releases
+    { type: 'major', release: 'major' },
+    { type: 'minor', release: 'minor' },
+    { type: 'patch', release: 'patch' },
+    // Main changes
+    { type: 'fix', release: 'patch' },
+    { type: 'feat', release: 'patch' },
+    {
+        type: 'feat',
+        scope: 'minor',
+        release: 'minor',
+    },
+    {
+        type: 'feat',
+        scope: 'major',
+        release: 'major',
+    },
+    { type: 'styles', release: 'patch' },
+    // Special changes
+    { type: 'chore', release: false },
+    { type: 'docs', release: false },
+    { type: 'docs', scope: 'readme', release: 'patch' },
+    { type: 'refactor', release: 'patch' },
+    { type: 'perf', release: 'patch' },
+    { type: 'tests', release: false },
+]
+
 export default {
     branches: ["main"],
     plugins: [
         [
             "@semantic-release/commit-analyzer",
             {
-                preset: "angular",
-                releaseRules: [
-                    { breaking: true, release: "major" },
-                    { type: "feat", release: "minor" },
-                    { type: "fix", release: "patch" },
-                    { type: "perf", release: "patch" },
-                ],
+                parserOpts: { noteKeywords: noteKeywords },
+                releaseRules: releaseRules,
+
             },
         ],
         "@semantic-release/release-notes-generator",
